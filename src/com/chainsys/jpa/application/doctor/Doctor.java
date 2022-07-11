5ad14c63f0ce92cece6e60a73d62cfd4b687c4ed
kalaiyarasan <kalaiyarasan.ayyappan@chainsys.com>
2022-07-11 16:43:43 +0530

    jpa

---
+++ src/com/chainsys/jpa/application/doctor/Doctor.java
@@ -0,0 +1,83 @@
+package com.chainsys.jpa.application.doctor;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Doctor")
+public class Doctor {
+    @Id
+    private int doctor_id;
+    private String name;
+    private Date dob;
+    private String speciality;
+    private String city;
+    private long phone_number;
+    @Column(name = "std_fees")
+    private float fees;
+
+    public int getDoctor_id() {
+        return doctor_id;
+    }
+
+    public void setDoctor_id(int doctor_id) {
+        this.doctor_id = doctor_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public long getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(long phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public float getFees() {
+        return fees;
+    }
+
+    public void setFees(float fees) {
+        this.fees = fees;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d, %s, %s, %s, %s, %d", doctor_id, name, dob, city, phone_number, fees);
+    }
+}
